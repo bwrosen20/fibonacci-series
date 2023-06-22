c9d32945c704c32afa628c8613a2c31e9e04995b
bwrosen20 <bwrosen20@gmail.com>
2023-06-21 20:37:45 -0400

    commit tree fbc414c370268fac031b2d95cd3ead39c7febf37

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,5 +1,19 @@
 function fibonacci(num) {
-  // type your code here
+  let i=1
+  let j=2
+  let b=0
+  if (num===0)
+    return 0
+  else if (num===1)
+    return 1
+  else{ 
+  for (let n=2; n<num; n++){
+    b=i
+    i=j
+    j=b+j
+  }
+  return i
+}
 }
 
 if (require.main === module) {
@@ -19,6 +33,13 @@ if (require.main === module) {
 }
 
 module.exports = fibonacci;
+
+
+const fourth=fibonacci(6)
+const second=fibonacci(2)
+
+console.log(fourth)
+
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
